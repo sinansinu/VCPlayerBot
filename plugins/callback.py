c584d05f7e92c5c98abb9f8dbaa780d0b43c380b
LXR YOUTUBER AMAL <90690553+LXR-YOUTUBER-AMAL@users.noreply.github.com>
2021-09-24 15:47:55 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -97,8 +97,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot'),
+                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/LXRYOUTUBEYT'),
+                InlineKeyboardButton('🧩 Source', url='https://github.com/LXR-YOUTUBER-AMAL/VCPlayerBot'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
